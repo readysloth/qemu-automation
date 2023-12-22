0c7ab73218ca5a4428e13783ccb64b139a807937
readysloth <readysloth@protonmail.com>
2023-12-22 12:20:52 +0300

    add(*.sh)

---
+++ launch_qemu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+qemu_drive_template() {
+  file="$1"
+  serial="$2"
+  bootindex="$3"
+
+  option="-drive file=$file"
+  option="$option,id=drive_$serial"
+  option="$option,if=none"
+  option="$option,format=raw"
+  option="$option,media=disk"
+
+  option="$option -device ahci,id=ahci_$serial"
+  option="$option -device scsi-hd,drive=drive_$serial"
+  option="$option,serial=device_$serial"
+  option="$option,bus=ahci_$serial.0"
+  option="$option,bootindex=$bootindex"
+
+  echo "$option"
+}
+
+QEMU_MONITOR_IP=127.0.0.1
+QEMU_MONITOR_PORT="$RANDOM"
+SCENARIO="$1"
+DISK="${2:--}"
+OUTPUT_FILE="${3:-output.log}"
+MEMORY="${4:-1024}"
+set_qemu_monitor $QEMU_MONITOR_IP:$QEMU_MONITOR_PORT
+
+QEMU_DEFAULT_OPTIONS="-m $MEMORY"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -enable-kvm"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -smp $(nproc)"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -monitor tcp:$QEMU_MONITOR_IP:$QEMU_MONITOR_PORT,server,nowait"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -netdev user,id=ssh_net,hostfwd=tcp:127.0.0.1:12222-:22"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -device e1000,netdev=ssh_net"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -chardev file,id=serial_port,path=$OUTPUT_FILE"
+QEMU_DEFAULT_OPTIONS="$QEMU_DEFAULT_OPTIONS -device pci-serial,chardev=serial_port"
+
+if [ "$DISK" != '-' ]
+then
+  TARGET_DISK_OPTS="$(qemu_drive_template "$TARGET_DISK" "$TARGET_DISK_$RANDOM" 1)"
+fi
+
+QEMU_OPTIONS="$QEMU_DEFAULT_OPTIONS $TARGET_DISK_OPTS"
+QEMU_OPTIONS="$(echo "$QEMU_OPTIONS" | tr '\n' ' ')"
+
+qemu-system-x86_64 $QEMU_OPTIONS &
+
+source "$SCENARIO"
+
+wait $(jobs -p)
---
+++ type_in.sh
@@ -0,0 +1,96 @@
+export QEMU_MONITOR_IP=
+export QEMU_MONITOR_PORT=
+export QEMU_SEND_CMD='"nc -q0 $QEMU_MONITOR_IP $QEMU_MONITOR_PORT"'
+export SEND_NEWLINE=true
+
+QSEND=
+
+set_qemu_monitor_ip() {
+  QEMU_MONITOR_IP="$1"
+  QSEND=$(eval echo "$QEMU_SEND_CMD")
+}
+
+
+set_qemu_monitor_port() {
+  QEMU_MONITOR_PORT="$1"
+  QSEND=$(eval echo "$QEMU_SEND_CMD")
+}
+
+
+set_qemu_monitor() {
+  set_qemu_monitor_ip "$(echo "$1" | cut -d: -f1)"
+  set_qemu_monitor_port "$(echo "$1" | cut -d: -f2)"
+  QSEND=$(eval echo "$QEMU_SEND_CMD")
+}
+
+
+exit_if_no_monitor_setup() {
+  if [ -z "$QEMU_MONITOR_IP" ] || [ -z "$QEMU_MONITOR_PORT" ]
+  then
+    echo "You must set both QEMU_MONITOR_IP ($QEMU_MONITOR_IP) and QEMU_MONITOR_PORT ($QEMU_MONITOR_PORT)" >&2
+    echo "Use set_qemu_monitor_ip, set_qemu_monitor_port or set_qemu_monitor functions for that" >&2
+    exit 1
+  fi
+}
+
+
+type_in() {
+  exit_if_no_monitor_setup
+
+  do_inline=false
+  for ch in $(echo "$@" | sed 's/./& /g' | sed 's/*/\\*/g' )
+  do
+    if [ "$ch" = '`' ]
+    then
+      if [ "$do_inline" = 'true' ]
+      then
+        do_inline=false
+        echo $c | $QSEND
+        sleep 0.1
+        continue
+      fi
+      do_inline=true
+      c=
+      continue
+    fi
+
+    if [ "$do_inline" = "true" ]
+    then
+      [ "$ch" = '%' ] && c="$c " || c="$c$ch"
+    else
+      c="$ch"
+      case "$ch" in
+        '%') c=spc ;;
+        '-') c=minus ;;
+        '/') c=slash ;;
+        '\') c=backslash ;;
+        '.') c=dot ;;
+        '>') c=shift-dot ;;
+        '<') c=shift-comma ;;
+        '=') c=equal ;;
+        "'") c=apostrophe ;;
+        '"') c=shift-apostrophe ;;
+        '\*') c=asterisk ;;
+        '_') echo "sendkey shift-minus" | $QSEND && continue ;;
+        '~') DO_UPPERCASE=yes && continue ;;
+      esac
+
+      [ "$DO_UPPERCASE" == yes ] && DO_UPPERCASE= && c="shift-$c"
+      echo "sendkey $c" | $QSEND
+      sleep 0.1
+
+      [ -n "$TYPE_IN_LABMDA" ] && $TYPE_IN_LABMDA "$c"
+    fi
+  done
+  [ "$SEND_NEWLINE" = "true" ] && echo "sendkey kp_enter" | $QSEND
+}
+
+
+send_cmd() {
+  exit_if_no_monitor_setup
+
+  cmd="$@"
+  echo "$cmd" | $QSEND
+  sleep 0.1
+  [ -n "$SEND_CMD_LABMDA" ] && $SEND_CMD_LABMDA "$cmd"
+}
